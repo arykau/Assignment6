a3d6a5389db7524a1543dd3cf21c9180b00567c0
arykau <74668998+arykau@users.noreply.github.com>
2021-03-07 16:25:54 +0600

    Update LogisticsTest.java

--- LogisticsTest.java
+++ LogisticsTest.java
@@ -6,24 +6,21 @@ public class LogisticsTest {
 
     private static Logistics logistics;
 
-    static Scanner sc = new Scanner(System.in);
-    static String choice = "";
-
     public static void main(String[] args) {
-        config();
-        logisticsDeliver();
+        configuration();
+        logisticsService();
     }
 
-    static void config(){
-        System.out.println("Choose the type of transport (truck or ship):");
-        choice = sc.nextLine();
-        switch (choice) {
-            case ("truck") -> logistics = new RoadLogistics();
-            case ("ship") -> logistics = new SeaLogistics();
+    static void configuration(){
+        String transport = "truck";
+        if (transport.equals("truck")){
+            logistics = new RoadLogistics();
+        }else if (transport.equals("ship")){
+            logistics = new SeaLogistics();
         }
     }
 
-    static void logisticsDeliver(){
+    static void logisticsService(){
         logistics.planDelivery();
     }
 }
