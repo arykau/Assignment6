78c1d68b094f5de6ec7c6067eba0cb2b84c5be88
arykau <74668998+arykau@users.noreply.github.com>
2021-03-07 16:23:36 +0600

    Update Logistics.java

--- Logistics.java
+++ Logistics.java
@@ -1,10 +1,29 @@
 package subtask2;
 
-public abstract class Logistics {
-    public void planDelivery(){
-        Transport t = createTransport();
-        t.deliver();
+import java.util.Scanner;
+
+public class LogisticsTest {
+
+    private static Logistics logistics;
+
+    static Scanner sc = new Scanner(System.in);
+    static String choice = "";
+
+    public static void main(String[] args) {
+        config();
+        logisticsDeliver();
     }
 
-    public abstract Transport createTransport();
+    static void config(){
+        System.out.println("Choose the type of transport (truck or ship):");
+        choice = sc.nextLine();
+        switch (choice) {
+            case ("truck") -> logistics = new RoadLogistics();
+            case ("ship") -> logistics = new SeaLogistics();
+        }
+    }
+
+    static void logisticsDeliver(){
+        logistics.planDelivery();
+    }
 }
